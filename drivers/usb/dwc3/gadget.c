21ba6c25246932f964a68bc916a0b746fe1f6c28
Bar Weiner <bweiner@codeaurora.org>
2014-05-29 14:39:04 +0300

    usb: dwc3: support LPM during usb bus suspend
    This change implements the required sequence for going
    in and out of low-power-mode during usb bus suspend.
    
    It is also required to take the core out of LPM in case
    ep_queue is called by the upper layers. In this case,
    remote wakeup sequence will be initiated once the core
    is out of LPM.
    
    CRs-Fixed: 641671
    Change-Id: I8076e58db4037502589f04f8cd7e7fd98cdc59f4
    Signed-off-by: Bar Weiner <bweiner@codeaurora.org>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1349,8 +1349,23 @@ static int dwc3_gadget_ep_queue(struct usb_ep *ep, struct usb_request *request,
 	struct dwc3			*dwc = dep->dwc;
 
 	unsigned long			flags;
-
+	enum dwc3_link_state		link_state;
 	int				ret;
+	bool wakeup = false;
+
+	if (atomic_read(&dwc->in_lpm)) {
+		wakeup = true;
+	} else {
+		link_state = dwc3_get_link_state(dwc);
+		if (link_state == DWC3_LINK_STATE_RX_DET ||
+			link_state == DWC3_LINK_STATE_U3)
+			wakeup = true;
+	}
+
+	if (wakeup) {
+		dwc3_gadget_wakeup(&dwc->gadget);
+		return -EAGAIN;
+	}
 
 	spin_lock_irqsave(&dwc->lock, flags);
 
@@ -1384,6 +1399,11 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 
 	unsigned long			flags;
 	int				ret = 0;
+
+	if (atomic_read(&dwc->in_lpm)) {
+		dev_err(dwc->dev, "Unable to dequeue while in LPM\n");
+		return -EAGAIN;
+	}
 
 	spin_lock_irqsave(&dwc->lock, flags);
 
@@ -2805,6 +2825,8 @@ static void dwc3_gadget_conndone_interrupt(struct dwc3 *dwc)
 		return;
 	}
 
+	dwc3_notify_event(dwc, DWC3_CONTROLLER_CONNDONE_EVENT);
+
 	/*
 	 * Configure PHY via GUSB3PIPECTLn if required.
 	 *
@@ -2932,6 +2954,7 @@ static void dwc3_gadget_suspend_interrupt(struct dwc3 *dwc,
 		spin_unlock(&dwc->lock);
 		dwc->gadget_driver->suspend(&dwc->gadget);
 		spin_lock(&dwc->lock);
+		pm_runtime_put(dwc->dev);
 	}
 
 	dwc->link_state = next;
