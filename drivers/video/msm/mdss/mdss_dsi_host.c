44f60281d2a32580649b07da35f5949d601b8c8b
Kuogee Hsieh <khsieh@codeaurora.org>
2014-06-30 17:32:59 -0700

    msm: mdss: always set mdp_busy when partial update is disabled
    mdp_busy flag is used to serialze kickoff operations by ensuring
    that any pending kickoffs are first completed before issuing any
    new kickoffs. This avoids any potential mpd_busy_wait timeouts.
    Therefore, for normal operations (partial update disabled), mdp_busy
    flag should always be set. However, when partial update is enabled,
    width and height of panel's ROI need to be checked before setting
    mdp_busy flag. Otherwise mdp_busy_wait will timeout since no update
    will be sent to the panel. In addition, perform roi merge from both
    panels if roi-merge is enabled to decide whether column and page address
    has changed. If it was changed, then new column and page address need
    to be sent to the panel.
    
    CRs-Fixed: 692382
    Change-Id: Ib0070b84a626afffd4c51a65f59bc97aae1d8994
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -1600,6 +1600,8 @@ int mdss_dsi_cmdlist_rx(struct mdss_dsi_ctrl_pdata *ctrl,
 int mdss_dsi_cmdlist_commit(struct mdss_dsi_ctrl_pdata *ctrl, int from_mdp)
 {
 	struct dcs_cmd_req *req;
+	struct mdss_panel_info *pinfo;
+	struct mdss_rect *roi = NULL;
 	int ret = -EINVAL;
 	int rc = 0;
 
@@ -1664,8 +1666,19 @@ int mdss_dsi_cmdlist_commit(struct mdss_dsi_ctrl_pdata *ctrl, int from_mdp)
 need_lock:
 
 	if (from_mdp) { /* from pipe_commit */
-		/* acquire lock only has new frame update */
-		if (ctrl->roi.w != 0 || ctrl->roi.h != 0)
+		/*
+		 * when partial update enabled, the roi of pinfo
+		 * is updated before mdp kickoff. Either width or
+		 * height of roi is 0, then it is false kickoff so
+		 * no mdp_busy flag set needed.
+		 * when partial update disabled, mdp_busy flag
+		 * alway set.
+		 */
+		pinfo = &ctrl->panel_data.panel_info;
+		if (pinfo->partial_update_enabled)
+			roi = &pinfo->roi;
+
+		if (!roi || (roi->w != 0 || roi->h != 0))
 			mdss_dsi_cmd_mdp_start(ctrl);
 	}
 
