5056f66b836e4db41276ca8f2b1f10879687894c
Venkat Gopalakrishnan <venkatg@codeaurora.org>
2014-07-28 11:53:29 -0700

    mmc: core: Fix power class config for HS400
    Use the correct power class field from the extended CSD register
    for HS400 mode as defined in the eMMC5.0 specification.
    
    CRs-fixed: 690341
    Change-Id: Ie10e35941fd3c6ee49c686f721bf5af6fcd74862
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -273,8 +273,7 @@ struct _mmc_csd {
 #define EXT_CSD_POWER_OFF_LONG_TIME	247	/* RO */
 #define EXT_CSD_GENERIC_CMD6_TIME	248	/* RO */
 #define EXT_CSD_CACHE_SIZE		249	/* RO, 4 bytes */
-#define EXT_CSD_PWR_CL_DDR_200_195	253	/* RO */
-#define EXT_CSD_PWR_CL_DDR_200_360	254	/* RO */
+#define EXT_CSD_PWR_CL_DDR_200_360	253	/* RO */
 #ifdef CONFIG_HUAWEI_DSM
 #define EXT_CSD_PRE_EOL_INFO		267	/* RO */
 #define EXT_CSD_DEVICE_LIFE_TIME_EST_TYP_A		268	/* RO */
