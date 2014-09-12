fd536a5d54f8b534482069aa7e72d2980b63115d
Sudheer Papothi <spapothi@codeaurora.org>
2014-09-12 21:40:09 +0530

    ASoC: wcd9xxx: Set chopper settings for UHQA mode
    UHQA (Ultra High Quality Audio) requires chopper to set the
    frequency to the actual frequency. Change enables chopper
    to set the frequency to half for all audio streams
    with different sampling rates and 24-bits per sample
    other than UHQA mode.
    
    CRs-Fixed: 722714
    Change-Id: I7dbf2629c37f0139af67c8491a49197bb85cd315
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>

--- sound/soc/codecs/wcd9330.c
+++ sound/soc/codecs/wcd9330.c
@@ -3908,6 +3908,7 @@ static int tomtom_hphl_dac_event(struct snd_soc_dapm_widget *w,
 						 WCD9XXX_CLSH_EVENT_PRE_DAC);
 		} else {
 			wcd9xxx_enable_high_perf_mode(codec, &tomtom_p->clsh_d,
+						tomtom_p->uhqa_mode,
 						WCD9XXX_CLSAB_STATE_HPHL,
 						WCD9XXX_CLSAB_REQ_ENABLE);
 		}
@@ -3935,6 +3936,7 @@ static int tomtom_hphl_dac_event(struct snd_soc_dapm_widget *w,
 						 WCD9XXX_CLSH_EVENT_POST_PA);
 		} else {
 			wcd9xxx_enable_high_perf_mode(codec, &tomtom_p->clsh_d,
+						tomtom_p->uhqa_mode,
 						WCD9XXX_CLSAB_STATE_HPHL,
 						WCD9XXX_CLSAB_REQ_DISABLE);
 		}
@@ -3961,6 +3963,7 @@ static int tomtom_hphr_dac_event(struct snd_soc_dapm_widget *w,
 						 WCD9XXX_CLSH_EVENT_PRE_DAC);
 		} else {
 			wcd9xxx_enable_high_perf_mode(codec, &tomtom_p->clsh_d,
+						tomtom_p->uhqa_mode,
 						WCD9XXX_CLSAB_STATE_HPHR,
 						WCD9XXX_CLSAB_REQ_ENABLE);
 		}
@@ -3982,6 +3985,7 @@ static int tomtom_hphr_dac_event(struct snd_soc_dapm_widget *w,
 						 WCD9XXX_CLSH_EVENT_POST_PA);
 		} else {
 			wcd9xxx_enable_high_perf_mode(codec, &tomtom_p->clsh_d,
+						tomtom_p->uhqa_mode,
 						WCD9XXX_CLSAB_STATE_HPHR,
 						WCD9XXX_CLSAB_REQ_DISABLE);
 		}
