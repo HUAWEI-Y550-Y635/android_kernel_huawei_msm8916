39eaa35b110981d1ad20cf06b9e2282547a4a823
Ryan Pennucci <decimalman@gmail.com>
2014-09-24 22:33:00 -0400

    arm: unsafe bitops "optimizations"
    These are beneficial iff more than a few percent of loads and/or stores
    can be avoided.
    
    Conflicts:
    	arch/arm/lib/bitops.h
    
    Conflicts:
    	arch/arm/lib/bitops.h

--- arch/arm/lib/bitops.h
+++ arch/arm/lib/bitops.h
@@ -1,7 +1,19 @@
 #include <asm/unwind.h>
 
 #if __LINUX_ARM_ARCH__ >= 6
-	.macro	bitop, name, instr
+/* TODO: beneficial to ldr before ldrex? */
+/* revb: branch with reversed logic */
+	.macro	revb, cond, tgt
+	.ifc \cond,"eq"
+	bne	\tgt
+	.else
+	.ifc \cond,"ne"
+	beq	\tgt
+	.endif
+	.endif
+	.endm
+
+	.macro	bitop, name, instr, cond
 ENTRY(	\name		)
 UNWIND(	.fnstart	)
 	ands	ip, r1, #3
@@ -11,17 +23,26 @@ UNWIND(	.fnstart	)
 	mov	r0, r0, lsr #5
 	add	r1, r1, r0, lsl #2	@ Get word offset
 	mov	r3, r2, lsl r3
+.ifnc \cond,""
+	ldr	r2, [r1]
+	tst	r2, r3
+	revb	\cond, 2f
+.endif
 1:	ldrex	r2, [r1]
+.ifnc \cond,""
+	tst	r2, r3
+.endif
 	\instr	r2, r2, r3
+	revb	\cond, 2f
 	strex	r0, r2, [r1]
 	cmp	r0, #0
 	bne	1b
-	bx	lr
+2:	bx	lr
 UNWIND(	.fnend		)
 ENDPROC(\name		)
 	.endm
 
-	.macro	testop, name, instr, store
+	.macro	testop, name, instr, cond
 ENTRY(	\name		)
 UNWIND(	.fnstart	)
 	ands	ip, r1, #3
@@ -31,22 +52,29 @@ UNWIND(	.fnstart	)
 	mov	r0, r0, lsr #5
 	add	r1, r1, r0, lsl #2	@ Get word offset
 	mov	r3, r2, lsl r3		@ create mask
+	mov	ip, #0
 	smp_dmb
+.ifnc \cond,""
+	ldr	r2, [r1]
+	ands	r0, r2, r3
+	revb	\cond, 2f
+.endif
 1:	ldrex	r2, [r1]
 	ands	r0, r2, r3		@ save old value of bit
 	\instr	r2, r2, r3		@ toggle bit
+	revb	\cond, 2f
 	strex	ip, r2, [r1]
 	cmp	ip, #0
 	bne	1b
 	smp_dmb
-	cmp	r0, #0
+2:	cmp	r0, #0
 	movne	r0, #1
-2:	bx	lr
+	bx	lr
 UNWIND(	.fnend		)
 ENDPROC(\name		)
 	.endm
 #else
-	.macro	bitop, name, instr
+	.macro	bitop, name, instr, cond
 ENTRY(	\name		)
 UNWIND(	.fnstart	)
 	ands	ip, r1, #3
@@ -73,7 +101,7 @@ ENDPROC(\name		)
  * Note: we can trivially conditionalise the store instruction
  * to avoid dirtying the data cache.
  */
-	.macro	testop, name, instr, store
+	.macro	testop, name, instr, cond
 ENTRY(	\name		)
 UNWIND(	.fnstart	)
 	ands	ip, r1, #3
@@ -85,7 +113,7 @@ UNWIND(	.fnstart	)
 	mov	r0, #1
 	tst	r2, r0, lsl r3
 	\instr	r2, r2, r0, lsl r3
-	\store	r2, [r1]
+	str\cond	r2, [r1]
 	moveq	r0, #0
 	restore_irqs ip
 	mov	pc, lr
