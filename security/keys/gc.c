34b282839f3ebe3d0830bf6f4b8971833b040dd8
CarlosArriagaCM <CarlosArriagaCM@gmail.com>
2016-09-19 18:08:58 -0500

    fix

--- security/keys/gc.c
+++ security/keys/gc.c
@@ -207,13 +207,10 @@ static noinline void key_gc_unused_keys(struct list_head *keys)
 		if (test_bit(KEY_FLAG_INSTANTIATED, &key->flags))
 			atomic_dec(&key->user->nikeys);
 
-<<<<<<< HEAD
-=======
 		/* now throw away the key memory */
 		if (key->type->destroy)
 			key->type->destroy(key);
 
->>>>>>> ffc5f7c... KEYS: close race between key lookup and freeing
 		key_user_put(key->user);
 
 		kfree(key->description);
