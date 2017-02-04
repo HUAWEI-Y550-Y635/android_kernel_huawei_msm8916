f865502aa86be14e4842600ed8912770fdb41c14
kabirkalsi <kabir_7@hotmail.com>
2017-02-04 17:05:19 +0000

    Decrease Time To Enter Sleep
    Signed-off-by: rishabhrao <admin@rishabhrao.net>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -25,7 +25,7 @@ extern int suspend_sys_sync_wait(void);
 /* 
  * Timeout for stopping processes
  */
-unsigned int __read_mostly freeze_timeout_msecs = 20 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 2 * MSEC_PER_SEC;
 
 static int try_to_freeze_tasks(bool user_only)
 {
